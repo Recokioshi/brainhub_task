49b262bacb736fabc25efea6fc21b011c1677fbc
recokioshi <recokioshi@gmail.com>
2020-06-12 22:50:44 +0200

    -added redux to project -created empty EventForm component

---
+++ client/src/components/EventForm/EventForm.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const EventForm: React.FC = () => {
+  return <div>EventForm</div>;
+};
+
+export default connect()(EventForm);
---
+++ client/src/redux/reducer.ts
@@ -0,0 +1,7 @@
+import { Reducer } from 'redux';
+
+const reducer: Reducer = () => {
+  return {};
+};
+
+export default reducer;
